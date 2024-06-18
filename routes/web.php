8b9fa15f5d03462a8e8f41487f9b77bdf4eb63c2
al-kirpichenko <fuluganchik@yandex.ru>
2024-06-19 03:21:52 +0700

    создан контроллер админки

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route:: get ('/admin', [AdminController:: class , 'admin'])->name('admin')->middleware('is_admin');
